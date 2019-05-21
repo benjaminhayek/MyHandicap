5c86779d05d28f18ff2cc0d94baeddfca1c3f3a2
bhayek12@gmail.com <bhayek12@gmail.com>
2019-05-21 13:51:04 -0600

    Get basic App runnin but need to add OAuth

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react'
+
 import { StyleSheet, Platform, Image, Text, View } from 'react-native'
-import { SwitchNavigator } from 'react-navigation'
-// import the different screens
+
+import { createSwitchNavigator, createAppContainer } from 'react-navigation'
+
 import Loading from './Loading'
 import SignUp from './Signup'
 import Login from './Login'
 import Main from './Main'
-// create our app's navigation stack
-const App = SwitchNavigator(
-  {
-    Loading,
-    SignUp,
-    Login,
-    Main
-  },
-  {
-    initialRouteName: 'Loading'
-  }
-)
-export default App
+
+export default createAppContainer(createSwitchNavigator(
+
+{
+  Loading,
+  SignUp,
+  Login,
+  Main
+},
+{
+  initialRouteName: 'Loading'
+}
+
+));
