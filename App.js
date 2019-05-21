78c0cf558ed0f9d0b5ef7f21104e011539437055
bhayek12@gmail.com <bhayek12@gmail.com>
2019-05-21 12:01:20 -0600

    Delete boilerplate

--- App.js
+++ App.js
@@ -9,21 +9,12 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Welcome to MyHandicap!</Text>
       </View>
     );
   }
@@ -32,14 +23,16 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    marginTop: 30,
     textAlign: 'center',
-    margin: 10,
+    backgroundColor: 'green',
+    height: 30,
   },
   instructions: {
     textAlign: 'center',
