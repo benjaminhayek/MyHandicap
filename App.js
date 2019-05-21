3c8c81ce8febbbb634df7b13152c723fd75624ea
bhayek12@gmail.com <bhayek12@gmail.com>
2019-05-21 12:42:12 -0600

    update header

--- App.js
+++ App.js
@@ -9,6 +9,13 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
 type Props = {};
 export default class App extends Component<Props> {
   render() {
@@ -22,21 +29,15 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
+    flex: 1,
     fontSize: 20,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
+    textAlign: 'center',
     marginTop: 30,
-    textAlign: 'center',
-    backgroundColor: 'green',
-    height: 30,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
