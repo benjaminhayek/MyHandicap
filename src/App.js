e353bf584648ce4200abc40a5e76013f75bda53c
bhayek12@gmail.com <bhayek12@gmail.com>
2019-05-22 11:57:25 -0600

    Restrucure app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
-
 import { StyleSheet, Platform, Image, Text, View } from 'react-native'
-
 import { createSwitchNavigator, createAppContainer } from 'react-navigation'
 
 import Loading from './Loading'
