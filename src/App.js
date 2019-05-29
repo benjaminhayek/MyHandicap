8e27f6a1a951fe8829808f7c26a14cc9c2f79a5b
bhayek12@gmail.com <bhayek12@gmail.com>
2019-05-29 11:27:04 -0600

    Add react navigation to initial route of login

--- src/App.js
+++ src/App.js
@@ -1,96 +1,15 @@
-import React from "react";
-import { StyleSheet, Text, View, AsyncStorage, Image } from "react-native";
-import { Container, Item, Form, Input, Button, Label } from "native-base";
-import * as firebase from "firebase";
-import {firebaseConfig} from './config'
-import { Logo } from './images';
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+import React from 'react'
+import { StyleSheet, Platform, Image, Text, View } from 'react-native'
+import { createSwitchNavigator, createAppContainer } from 'react-navigation'
+import Login from './Login'
 
-    this.state = {
-      email: "",
-      password: "",
-    };
-  }
-  SignUp = (email, password) => {
-    try {
-      firebase.auth().createUserWithEmailAndPassword(email, password);
-    } catch (error) {
-      alert(firebaseConfig);
-    }
-  };
-  SignIn = (email, password) => {
-    try {
-      firebase.auth().signInWithEmailAndPassword(email, password);
-      firebase.auth().onAuthStateChanged(user => {
-         alert(user.email);
-      })
-} catch (error) {
-      console.log(error.toString(error));
-    }
-  };
-  render() {
-    return (
-      <Container style={styles.container}>
-        <Image source={ Logo } style={{width: 250, height: 250, marginLeft: 50}}/>
-        <Text style={styles.header}>
-          Welcome to MyHandicap!
-        </Text>
-        <Form>
-          <Item floatingLabel>
-            <Label>Email</Label>
-            <Input
-              autoCapitalize="none"
-              autoCorrect={false}
-              onChangeText={email => this.setState({ email })}
-            />
-          </Item>
-          <Item floatingLabel>
-            <Label>Password</Label>
-            <Input
-              secureTextEntry={true}
-              autoCapitalize="none"
-              autoCorrect={false}
-              onChangeText={password => this.setState({ password })}
-            />
-          </Item>
-          <Button
-            full
-            rounded
-            style={{ marginTop: 20 }}
-            onPress={() => this.SignIn(this.state.email, this.state.password)}
-          >
-            <Text>SignIn</Text>
-          </Button>
-          <Button
-            full
-            rounded
-            success
-            style={{ marginTop: 20 }}
-            onPress={() => this.SignUp(this.state.email, this.state.password)}
-          >
-            <Text>Signup</Text>
-          </Button>
-        </Form>
-      </Container>
-    );
-  }
-}
+// create our app’s navigation stack
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    // alignItems: "center",
-    justifyContent: "center",
-    padding: 10
+export default createAppContainer(createSwitchNavigator(
+  {
+    Login,
   },
-  header: {
-    textAlign: 'center',
-    fontSize: 25,
-    color: 'green'
-  }
-});
+  {
+    initialRouteName: 'Login'
+  }   
+));
