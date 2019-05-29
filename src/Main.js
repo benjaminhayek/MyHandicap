37ef8ba5f8f1e2303b82b849e6d5ae47f84faf45
bhayek12@gmail.com <bhayek12@gmail.com>
2019-05-29 11:59:23 -0600

    Add post score method

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import { StyleSheet, Platform, Image, Text, View } from 'react-native'
+import { StyleSheet, Platform, Image, Text, View, Form, Button, Item, Label, Input } from 'react-native'
 import firebase from 'firebase'
+import { Logo } from './images';
 
 export default class Main extends React.Component {
-  state = { currentUser: null }
+  state = { 
+      currentUser: null,
+      course: '',
+      score: '',
+      courseRating: '',
+      slope: '',
+      divisor: 113 
+    }
 
   componentDidMount() {
     const { currentUser } = firebase.auth()
@@ -11,14 +19,48 @@ export default class Main extends React.Component {
     this.setState({ currentUser })
   }
 
+  postScore = (currentUser, course, score, courseRating, slope, divisor) => {
+    alert(currentUser, course, score, courseRating, slope, divisor)
+  };
+
   render() {
-    const { currentUser } = this.state
+    const { currentUser, course, score, courseRating, slope, divisor } = this.state
 
     return (
       <View style={styles.container}>
+        <Image source={ Logo } style={{width: 250, height: 250, }}/>
         <Text>
-          Hi {currentUser && currentUser.email}! Welcome to MyHandicap!
+          Hi {currentUser && currentUser.email}! Welcome to MyHandicap! Please enter your information below
         </Text>
+        <Form>
+          <Item floatingLabel>
+            <Label>Course</Label>
+            <Input
+              autoCapitalize="none"
+              autoCorrect={false}
+              onChangeText={course => this.setState({ course })}
+            />
+          </Item>
+          <Item floatingLabel>
+            <Label>Course Rating</Label>
+            <Input
+              secureTextEntry={true}
+              autoCapitalize="none"
+              autoCorrect={false}
+              onChangeText={courseRating => this.setState({ courseRating })}
+            />
+          </Item>
+            <Text>SignIn</Text>
+          <Button
+            full
+            rounded
+            success
+            style={{ marginTop: 20 }}
+            onPress={() => this.postScore(this.state.email, this.state.password)}
+          >
+            <Text>Post Score</Text>
+          </Button>
+        </Form>
       </View>
     )
   }
